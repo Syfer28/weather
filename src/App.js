711de4bc495a6cb883eddb5d39c80dc187e52210
ElbekRahmanov@yandex.ru <ElbekRahmanov@yandex.ru>
2024-05-27 00:04:33 +0300

    google maps api + openweather api added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import styles from "./styles/App.module.css";
+import Map from "./component/Map";
+import Weather from "./component/Weather";
+
+function App() {
+  const [coord, setCoord] = useState({});
+  return (
+    <div className={styles.container}>
+      <Map setCoord={setCoord} />
+      <Weather coord={coord} />
+    </div>
+  );
+}
+
+export default App;
