91fb848dda4fe54f827a593c35ab3946aee8ab09
Elbek Rakhmanov <rakhelbek@gmail.com>
2024-05-28 01:37:23 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Weather from "./component/Weather";
 function App() {
   const [coord, setCoord] = useState({});
   return (
-    <div className={styles.container}>
-      <Map setCoord={setCoord} />
-      <Weather coord={coord} />
+    <div className={styles.App}>
+      <div className={styles.container}>
+        <Map setCoord={setCoord} />
+        <Weather coord={coord} />
+      </div>
     </div>
   );
 }
